bdc71f3faae3776122a20821843541adb329a367
aleksanderesk <aleksanderesk@gmail.com>
2016-02-20 06:35:13 -0600

    updating handler for templates

--- src/causal/handler.clj
+++ src/causal/handler.clj
@@ -1,12 +1,17 @@
 (ns causal.handler
   (:require [compojure.core :refer [defroutes routes]]
+            [ring.util.response :refer [redirect]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
             [ring.middleware.resource :refer [wrap-resource]]
             [ring.middleware.file-info :refer [wrap-file-info]]
             [hiccup.middleware :refer [wrap-base-url]]
             [compojure.handler :as handler]
             [compojure.route :as route]
+            [buddy.auth :refer [authenticated?]]
+            [buddy.auth.backends.session :refer [session-backend]]
+            [buddy.auth.middleware :refer [wrap-authentication wrap-authorization]]
             [causal.models.migrations :refer [migrate]]
-            [causal.routes.home :refer [home-routes]]))
+            [causal.routes.base :refer [base-routes]]))
 
 (defn init []
   (println "causal is starting")
@@ -16,11 +21,26 @@
 (defn destroy []
   (println "causal is shutting down"))
 
+(defn unauthorized-handler
+  [request metadata]
+  (cond
+    (authenticated? request) (-> (redirect "/error-403")
+                                 (assoc :status 403))
+    :else (let [current-url (:uri request)]
+            (redirect (format "/login?next=%s" current-url)))))
+
+(def auth-backend
+  (session-backend {:unauthorized-handler unauthorized-handler}))
+
 (defroutes app-routes
   (route/resources "/")
-  (route/not-found "Not Found"))
+  (route/not-found (redirect "/error-404")))
 
 (def app
-  (-> (routes home-routes app-routes)
+  (-> (routes base-routes app-routes)
+      (wrap-authorization auth-backend)
+      (wrap-authentication auth-backend)
+      (wrap-json-body {:keywords? true})
+      (wrap-json-response)
       (handler/site)
       (wrap-base-url)))
